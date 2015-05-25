22f52551b00468deb9d25be280572ba892404c5f
juanmnl <juanmnlcornejo@gmail.com>
2015-05-25 12:19:34 -0500

    Add static pages

--- models/route.js
+++ models/route.js
@@ -1,3 +1,20 @@
+
 Router.route("/", function(){
   this.render("home");
+});
+
+Router.route("/projects", function(){
+  this.render("projects");
+});
+
+Router.route("/ideas", function(){
+  this.render("ideas");
+});
+
+Router.route("/photography", function(){
+  this.render("photography");
+});
+
+Router.route("/words", function(){
+  this.render("words");
 });
