79697ca11294d4c0672b6635ff1f2ecdfb880201
juanmnl <juanmnlcornejo@gmail.com>
2015-07-31 18:47:30 -0500

    Fix final color palette, add selection pseudo class style

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function() {
   $("#screen .text").typed({
-    strings: ["Hola", "^500 Bienvenido a 1984... ^1000", "^1000 Quieres ^500 jugar un pequeño juego? ^1000", ";)", "Te atreves? ^500", "1) Si<br>2) No<br>" ],
+    strings: ["Hola ^1000", "^500 Bienvenid@ a mi laberinto ^1000", "^1000 Quieres ^800 jugar un pequeño juego? ^1000", ";)", "Te atreves? ^1000", "1) Si<br>2) No<br>" ],
     contentType: 'html',
     typeSpeed: 5,
     startDelay: 3000,
