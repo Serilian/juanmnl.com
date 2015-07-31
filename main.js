19b3c6324b368cfb4529afa6ba1493a39fcc2611
juanmnl <juanmnlcornejo@gmail.com>
2015-07-31 17:55:27 -0500

    Add green and white styles

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 $(function() {
-  $(".element").typed({
-    strings: ["Hola ^1000", "Bienvenido a mi página web."],
-    typeSpeed: 5
+  $("#screen .text").typed({
+    strings: ["Hola", "^500 Bienvenido a 1984... ^1000", "^1000 Quieres ^500 jugar un pequeño juego? ^1000", ";)", "Te atreves? ^500", "1) Si<br>2) No<br>" ],
+    contentType: 'html',
+    typeSpeed: 5,
+    startDelay: 3000,
+    loop: false
   });
 });
