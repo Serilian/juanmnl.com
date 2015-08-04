e31b3982b250cdb9855c47cf65365ce654262677
juanmnl <juanmnlcornejo@gmail.com>
2015-08-03 21:12:20 -0500

    Fix welcome text and style

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function() {
   $("#screen .text").typed({
-    strings: ["<span class='danger'>WARNING!</span> ^1000", "^500 The system has been compromised ^1000", "^1000 Virus is spreading ^1000" ],
+    strings: ["<span class='danger'>WARNING!</span> ^1000", "^500 The system has been compromised ^1000", "^1000 <span class='danger'>– ACCESS DENIED –</span> <br> ^1000", "<span class='danger'>WARNING!</span> ^1000", "Installing Malware.................<br>................................... <br>................................... <br>................................... <br>................................... <br>................................... <br>................................... ^1000" ],
     contentType: 'html',
     typeSpeed: 5,
     startDelay: 3000,
