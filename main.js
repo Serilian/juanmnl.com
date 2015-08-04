286fc34db3e60c8aa429291ef4257b95c3b30aa9
juanmnl <juanmnlcornejo@gmail.com>
2015-08-03 20:35:20 -0500

    Add production style with warning

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $(function() {
   $("#screen .text").typed({
-    strings: ["Hola ^1000", "^500 Bienvenid@ a mi laberinto ^1000", "^1000 Quieres ^800 jugar un pequeño juego? ^1000", ";)", "Te atreves? ^1000", "1) Si<br>2) No<br>" ],
+    strings: ["<span class='danger'>WARNING!</span> ^1000", "^500 The system has been compromised ^1000", "^1000 Virus is spreading ^1000" ],
     contentType: 'html',
     typeSpeed: 5,
     startDelay: 3000,
-    loop: false
+    loop: true
   });
 });
