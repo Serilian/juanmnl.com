1ae69585310700cf418c77eec85e307a95b87db2
juanmnl <juanmnlcornejo@gmail.com>
2015-08-04 17:30:07 -0500

    Add imagemin for new images on build pipe

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     sass = require('gulp-sass'),
+    changed = require('gulp-changed'),
+    imagemin = require('gulp-imagemin'),
     browserSync = require('browser-sync');
 
 // BUILD STYLES FROM SRC SASS
@@ -8,6 +10,17 @@ gulp.task('styles', function() {
   gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./build/styles/'))
+});
+
+// MINIFY IMAGES NEW IMAGES
+gulp.task('imagemin', function() {
+  var imgSrc = './src/images/**/*',
+      imgDst = './build/images';
+
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
 });
 
 // WATCH TASK
