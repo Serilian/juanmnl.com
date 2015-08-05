644dafb07c9661137f206f3d4c30b82fbb814684
juanmnl <juanmnlcornejo@gmail.com>
2015-08-05 01:33:37 -0500

    Fix texts, typos and add yellow for warnings

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,17 @@
 $(function() {
   $("#screen .text").typed({
-    strings: ["<span class='danger'>WARNING!</span>", "<span class='danger'>WARNING!</span> ^1000", "Contact Admin ^1000", "^500 The system has been compromised ^1000", "^1000 <span class='danger'>–ACCESS DENIED–</span><br> ^1000", "<span class='danger'>WARNING!</span> ^1000", "Installing Malware ................<br>...................................<br>...................................<br>...................................<br>...................................", "I'm just joking!<br> This site is under construction ^1000", "Contact me on twitter or github <br> <i>(with on of the buttons below)</i> ^3000", ],
+    strings: [
+    "<span class='warning'>WARNING</span> ^1000",
+    "^1000 System has been compromised ^1000",
+    "^1000 <span class='danger'>***FATAL 3RR0R***</span><br>",
+    "<span class='danger'>DANGER!!!</span> ^1000",
+    "Installing Malware.................<br>...................................<br>...................................<br>...................................<br>...................................",
+    "I'm just joking!<br> This site is under construction ^1000",
+    "Contact me on twitter or github <br> <i>(with on of the buttons below)</i>"],
     contentType: 'html',
-    typeSpeed: 5,
+    typeSpeed: 2,
     backSpeed: 1,
-    startDelay: 3000,
-    loop: true
+    startDelay: 2000,
+    loop: false
   });
 });
