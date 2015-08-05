e1924482b865b87b6d7e6c632a36290aded0eb24
juanmnl <juanmnlcornejo@gmail.com>
2015-08-04 21:45:52 -0500

    Add divshot deploy config and build

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 $(function() {
   $("#screen .text").typed({
-    strings: ["<span class='danger'>WARNING!</span>", "<span class='danger'>WARNING!</span> ^1000", "Contact Admin ⬇︎⬇︎⬇︎ ^3000", "^500 The system has been compromised ^1000", "^1000 <span class='danger'>– ACCESS DENIED –</span> <br> ^1000", "<span class='danger'>WARNING!</span> ^1000", "Installing Malware.................<br>................................... <br>................................... <br>................................... <br>................................... <br>................................... <br>...................................", "I'm just joking!<br> This site is under construction ^1000" ],
+    strings: ["<span class='danger'>WARNING!</span>", "<span class='danger'>WARNING!</span> ^1000", "Contact Admin ^1000", "^500 The system has been compromised ^1000", "^1000 <span class='danger'>–ACCESS DENIED–</span><br> ^1000", "<span class='danger'>WARNING!</span> ^1000", "Installing Malware ................<br>...................................<br>...................................<br>...................................<br>...................................", "I'm just joking!<br> This site is under construction ^1000", "Contact me on twitter or github <br> <i>(with on of the buttons below)</i> ^3000", ],
     contentType: 'html',
     typeSpeed: 5,
     backSpeed: 1,
     startDelay: 3000,
-    loop: true,
-    cursorChar: "|"
+    loop: true
   });
 });
