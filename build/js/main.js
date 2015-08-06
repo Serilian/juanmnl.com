0c406799d199710373fcf42efcb96a1981bd5703
juanmnl <juanmnlcornejo@gmail.com>
2015-08-06 17:47:21 -0500

    Fix text and layout

--- build/js/main.js
+++ build/js/main.js
@@ -2,9 +2,8 @@ $(function() {
   $("#screen .text").typed({
     strings: [
     "Hello there! ^1000",
-    "^1000 My name is Juan Manuel Cornejo",
-    "I love to do web things :P ^1000",
-    "DESIGN",
+    "^1000 My name is Juan Manuel Cornejo<br>and I love to do web things :P ^1000",
+    "^1000 DESIGN",
     "RESEARCH and DEVELOPMENT",
     "UI <i><small>*User interface</small></i>",
     "UX <i><small>*User experience</small></i>",
@@ -12,13 +11,13 @@ $(function() {
     "PROTOTYPING",
     "WEB and MOBILE APPS",
     "^1000 This is an actual work-in-progress for an UI* research<br><i><small>*User interface</small></i> ^1000",
-    "<span class='warning'>(This site is under development and can behave erratically)</span> ^1000",
+    "<small><span class='warning'>Disclaimer **This site is under development<br>and can behave erratically**</span></small> ^1000",
     "If you want to get in touch, ^500 please use the buttons below. ^1000",
     "Or throw me an email juanmnl@me.com :)"],
     contentType: 'html',
-    typeSpeed: 1,
+    typeSpeed: 5,
     backSpeed: 2,
-    startDelay: 1013,
+    startDelay: 3000,
     loop: false
   });
 });
