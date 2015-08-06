34cba581c4450f463ed41aaea49cde235143ba26
juanmnl <juanmnlcornejo@gmail.com>
2015-08-06 16:20:11 -0500

    Fix js file

--- build/js/main.js
+++ build/js/main.js
@@ -3,12 +3,12 @@ $(function() {
     strings: [
     "Hello there! ^1000",
     "^1000 Welcome to my website ^1000",
-    "^1000 This is a work in progress for an UI research<br><i>UI - User interface</i>",
-    "This site is under development and can behave erratically ^1000",
-    "If you want to get in touch, please use the buttons below. ^1000",
+    "^1000 This is a work in progress for an UI* research<br><i>*UI - User interface</i>^1000",
+    "(This site is under development and can behave erratically) ^1000",
+    "If you want to get in touch, ^500 please use the buttons below. ^1000",
     "Or throw me an email juanmnl@me.com :)"],
     contentType: 'html',
-    typeSpeed: 5,
+    typeSpeed: 1,
     backSpeed: 2,
     startDelay: 2000,
     loop: false
