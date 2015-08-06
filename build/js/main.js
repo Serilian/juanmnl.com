b8cc23e12e8ae5f5460d49324082936320f3484e
juanmnl <juanmnlcornejo@gmail.com>
2015-08-06 17:24:26 -0500

    Fix and show warning color

--- build/js/main.js
+++ build/js/main.js
@@ -3,7 +3,7 @@ $(function() {
     strings: [
     "Hello there! ^1000",
     "^1000 My name is Juan Manuel Cornejo",
-    "I love the web ^1000",
+    "I love to do web things :P ^1000",
     "DESIGN",
     "RESEARCH and DEVELOPMENT",
     "UI <i><small>*User interface</small></i>",
@@ -12,7 +12,7 @@ $(function() {
     "PROTOTYPING",
     "WEB and MOBILE APPS",
     "^1000 This is an actual work-in-progress for an UI* research<br><i><small>*User interface</small></i> ^1000",
-    "(This site is under development and can behave erratically) ^1000",
+    "<span class='warning'>(This site is under development and can behave erratically)</span> ^1000",
     "If you want to get in touch, ^500 please use the buttons below. ^1000",
     "Or throw me an email juanmnl@me.com :)"],
     contentType: 'html',
