8a1a374ca3b7068b99b192ad596a60bcae4ad110
juanmnl <juanmnlcornejo@gmail.com>
2015-08-07 17:17:38 -0500

    Combed css, fixed one liner for welcome text WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,16 @@
-$(function () {
+$(document).ready(function() {
+  console.log("Hello Mofo!! I'll opensource this site very soon :) Don't forget to say hi");
+
   $("#screen .text").typed({
     strings: [
       '<small><span class="warning">**This site is under development**</span></small>',
-      'Hello there!^1000',
+      'Hello there! ;)^1000',
       'How are you?^1000',
-      '^1000 My name is Juan Manuel Cornejo and i\'m a frontend designer/engineer^2500',
-      'This are some of the things i do^1000',
-      '^500 - RESEARCH and DEVELOPMENT^1000<br> ^500 - UI ENGINEERING <i><small>(User interface)</small></i>^1000<br> ^500 - UX DESIGN <i><small>(User experience)</small></i>^1000<br> ^500 - PRODUCT DESIGN^1000 <br> ^500 - PROTOTYPE DEVELOPMENT^1000<br> ^500 - WEB/MOBILE CONSULTING^1500',
-      '^1000 This is an actual work-in-progress for an UI research i\'m working on^1500',
-      'If you want to get in touch,^500 please use one of the buttons below^500',
-      'Or throw me an email juanmnl@me.com^3000<br>Thank you for stopping by!^500<br>:)^1000<br> – Juanmnl'
+      '^500 My name is Juan Manuel Cornejo and i\'m a frontend designer/engineer.^2000<br><br>Some of the things i do include:^500<br><br> ^500 - RESEARCH and DEVELOPMENT^1000<br> ^500 - UI ENGINEERING <i><small>(User interface)</small></i>^1000<br> ^500 - UX DESIGN <i><small>(User experience)</small></i>^1000<br> ^500 - PRODUCT DESIGN^1000 <br> ^500 - PROTOTYPE DEVELOPMENT^1000<br> ^500 - WEB/MOBILE CONSULTING^1500<br><br> ^1000 (This is an actual work-in-progress of an UI research i\'m working on.)^1500<br><br> If you want to get in touch,^500 please use one of the buttons below,^500<br>or throw me an email juanmnl@me.com^3000<br><br>Thank you for stopping by!^500<br><br>:)<br><br>  – Juanmnl'
     ],
     contentType: 'html',
-    typeSpeed: 4,
-    backSpeed: 3,
+    typeSpeed: 0,
+    backSpeed: 1,
     startDelay: 1000,
     loop: false
   });
