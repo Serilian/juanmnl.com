80d07ad5efb67f3ca99f4eaf4e9aa9e8586b6205
juanmnl <juanmnlcornejo@gmail.com>
2015-08-21 18:11:14 -0500

    Removed time from about typing

--- src/js/main.js
+++ src/js/main.js
@@ -69,9 +69,9 @@ function initMachine() {
   var welcomeText = function() {
     $("#frame #screen .text span").typed({
       strings: [
-        'Hello there! :)^1000',
-        'How are you?^1000',
-        'My name is Juan Manuel Cornejo and i\'m a frontend designer/engineer.^2000<br>I currently work from Quito,^200 Ecuador. ^500 <small>(0.2333° S, 78.5167° W – UTC/GMT -5)^1500</small><br><br> ^500 I love building things for the web.^500 My areas of interest are:^500<br><br>- RESEARCH and DEVELOPMENT^1000<br>- UI ENGINEERING <i><small>(User interface)</small></i>^1000<br>- UX DESIGN <i><small>(User experience)</small></i>^1000<br>- PRODUCT DESIGN^1000 <br>- PROTOTYPE DEVELOPMENT^1000<br>- WEB/MOBILE CONSULTING^1500<br><br> ^1000 <small>This is a WIP (work-in-progress) of an UI research i\'m working on.^1500</small><br><br>If you want to get in touch,^500 please contact me on twitter^300<br>or throw me an email juanmnl@me.com^3000<br><br>Thank you for stopping by!^500<br><br>:P<br><br>  – Juanmnl^2000<br><br><small>** Please continue with the buttons on the right <i>(external links for now)</i> **</small>'
+        'Hello there! :)',
+        'How are you?',
+        'My name is Juan Manuel Cornejo and i\'m a frontend designer/engineer.<br>I currently work from Quito, Ecuador. <small>(0.2333° S, 78.5167° W – UTC/GMT -5)</small><br><br> I love building things for the web. My areas of interest are:<br><br>- RESEARCH and DEVELOPMENT<br>- UI ENGINEERING <i><small>(User interface)</small></i><br>- UX DESIGN <i><small>(User experience)</small></i><br>- PRODUCT DESIGN <br>- PROTOTYPE DEVELOPMENT<br>- WEB/MOBILE CONSULTING<br><br>  <small>This is a WIP (work-in-progress) of an UI research i\'m working on.</small><br><br>If you want to get in touch, please contact me on twitter<br>or throw me an email juanmnl@me.com<br><br>Thank you for stopping by!<br><br>:P<br><br>  – Juanmnl<br><br><small>** Please continue with the buttons on the right <i>(external links for now)</i> **</small>'
       ],
       contentType: 'html',
       loop: false,
